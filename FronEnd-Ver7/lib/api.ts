a19f61a0dbc0bbde2b064d733e1cdc452a3a47de
Kubgxy <hannarong.bon@spumail.net>
2025-05-27 22:00:17 +0700

    Fix : Edit Fect Data Mongo

--- FronEnd-Ver7/lib/api.ts
+++ FronEnd-Ver7/lib/api.ts
@@ -1,3 +1,3 @@
 
 // ใส่ URL ngrok หรือ Backend จริงของน้องตรงนี้
-export const API_BASEURL = "https://373e-2405-9800-b860-b89d-9856-5fcd-108e-7f46.ngrok-free.app"; 
+export const API_BASEURL = "https://accessory-forgotten-foods-void.trycloudflare.com"; 
